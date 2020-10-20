40e36a2567864bf63492e4318fe0e27789f59d12
wicho1001 <wichoguerrero@yahoo.com>
2020-10-20 08:40:58 -0500

    feat: Added tailwind to project

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import 'tailwindcss/tailwind.css';
+import Router from 'next/router';
+import withGa from 'next-ga';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default withGa('UA-169353371-1', Router)(MyApp);
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import link from "next/link";
 
 function IndexPage(props) {
   return (
-    <div>Hola nundo</div>
+    <div className="py-20 bg-primary-500 text-3xl font-exo">Hola nundo</div>
   )
 };
 
