2f567ede2a91f6e62ef039e577271c3b9ec705b9
wicho1001 <wichoguerrero@yahoo.com>
2020-10-22 00:35:42 -0500

    feat: Added new styles to navbar

--- components/shared/Navbar.js
+++ components/shared/Navbar.js
@@ -4,13 +4,13 @@ const Navbar = () => {
 
   return (
     <section className="px-6 py-3 flex justify-between items-center shadow-2xl">
-      <img src="https://via.placeholder.com/150" alt="" className="h-15"/>
+      <img src="/logo.png" alt="" className="h-15"/>
       <section className="flex items-center w-1/3 justify-between">
         <a className="text-surface-900 font-duru" href="/services">Services</a>
         <a className="text-surface-900 font-duru" href="/how">How we work</a>
         <a className="text-surface-900 font-duru" href="/about">About</a>
         <a className="text-surface-900 font-duru" href="/blog">Blog</a>
-        <button className="px-6 py-3 bg-secondary-300 rounded">
+        <button className="px-6 py-3 bg-gradient-to-r from-secondary-500 to-primary-500 rounded font-exo text-primary-50">
           Reach out
         </button>
       </section>
