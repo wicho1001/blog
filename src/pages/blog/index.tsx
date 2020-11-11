1e69d1b668239ea816a052072082b05932e0348e
wicho1001 <wichoguerrero@yahoo.com>
2020-11-10 22:22:02 -0600

    fix: Added new styles in blogs page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -8,22 +8,22 @@ const Index = () => {
     <section className="w-full">
       <Layout>
        <section className="overflow-x-hidden"> 
-        <section className="relative flex flex-row-reverse w-4/5 mx-auto mt-60 mb-20 ">
+        <section className="relative flex flex-row-reverse w-4/5 mx-auto h-container">
           <img className="absolute top-0 left-0 -z-1 -mt-76" src="./star_one.svg" alt=""/>
           <img className="absolute bottom-0 left-0 -z-1 -mb-54" src="./star_two.svg" alt=""/>
           <img className="absolute bottom-0 right-0 -z-1 -mt-80 mr-35" src="./star_three.svg" alt=""/>
-          <img className="" src="./Blog_one.svg" alt=""/>
-          <div className="w-2/5 flex flex-col">
-            <p className="flex flex-col text-5xl text-secondary-700 font-exo font-bold">
-              We Write For Future
-            </p>
-            <div className="w-4/5 flex flex-col">
+          <section className="flex mx-auto items-center">
+            <div className="flex flex-col">
+              <p className="text-5xl text-secondary-700 font-exo font-bold">
+                We Write For Future
+              </p>
               <p className="inline-flex font-rubik text-lg pt-4">
               "Lorem ipsum dolor sit amet consectetur adipisclaskje aliejd seda dof eliusmod tempor incuidoal
               nderit in voulpateate velt esse cillum dolores eu tugiat nuila pariatur"
               </p>
             </div>
-          </div>
+            <img className="w-8/12" src="./Blog_one.svg" alt=""/>
+          </section>
         </section>
 
           <section className="relative w-full">
