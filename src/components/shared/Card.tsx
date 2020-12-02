555e3ca9ce437dba12c79a8183a491c28cf8f674
Ivan Gutierrez <ing.iglds@gmail.com>
2020-12-01 20:56:50 -0600

    feat:Added new styles  and sections

--- src/components/shared/Card.tsx
+++ src/components/shared/Card.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({ props: {
+    author,
+    title,
+    featured_image,
+    slug,
+    fecha,
+  }}) => {
   return (
-    <section className="flex flex-col py-6 px-4 shadow-md col-span-1 rounded-2 hover:bg-surface-100 bg-white cursor-pointer">
+    <section className="flex inline-block flex-col py-6 px-4 shadow-md col-span-1 rounded-2 hover:bg-surface-100 bg-white cursor-pointer">
       <div className="flex items-center">
-        <img className="h-8 w-8 rounded mr-3" src="https://via.placeholder.com/150" alt=""/>
+        <img className="h-8 w-8 rounded mr-3" src={featured_image} alt=""/>
         <p className="font-duru text-surface-800">Php development</p>
       </div>
       <div className="flex flex-col px-6 mt-3">
-        <img className="w-full h-60 object-cover rounded-2" src="https://via.placeholder.com/500" alt=""/>
+        <img className="w-full h-60 object-cover rounded-2" src={featured_image} alt=""/>
         <section className="flex flex-col mt-3">
-          <p className="font-exo text-xl font-bold text-surface-800 text-center">Tips to Hire PHP Developer</p>
+          <p className="font-exo text-xl font-bold text-surface-800 text-center">{title}</p>
           <div className="flex justify-between items-center mt-6">
             <div className="flex items-center">
-              <img className="h-12 w-12 rounded-full shadow-md mr-3" src="https://via.placeholder.com/150" alt=""/>
+              <img className="h-12 w-12 rounded-full shadow-md mr-3" src={author.profile_image} alt=""/>
               <p className="font-rubik text-secondary-500 text-sm"><span className="text-surface-300">by</span> Wicho Guerrero</p>
             </div>
             <p className="font-rubik text-surface-300 text-sm">October 31, 2020</p>
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -2,20 +2,32 @@ import React, {useState, useEffect} from 'react';
 
 const Navbar = () => {
 
+    const menu=()=>{
+          var menu=document.getElementById("menu");
+          menu.style.display="block";
+    }
+
   return (
-    <section className="sticky bg-white top-0 h-20 px-6 flex justify-between items-center shadow-2xl z-2">
+    <section className="sticky  bg-white top-0 h-20 px-6 flex justify-between items-center shadow-2xl z-2">
       <img src="/logo.png" alt="" className="h-15"/>
-      <section className="flex items-center w-1/3 justify-between">
-        <a className="text-surface-900 font-duru" href="/services">Services</a>
-        <a className="text-surface-900 font-duru" href="/how">How we work</a>
-        <a className="text-surface-900 font-duru" href="/about">About</a>
-        <a className="text-surface-900 font-duru" href="/blog">Blog</a>
-        <button className="px-6 py-3 bg-gradient-to-r from-secondary-500 to-primary-500 rounded font-exo text-primary-50">
-          Reach out
-        </button>
-      </section>
+   
+        <div id="menu" className="hidden  md:flex md:items-center md:w-4/5 md:pl-15 lg:w-1/2 xl:w-1/3 justify-between">
+            
+            <a className="text-surface-900 font-duru" href="/services">Services</a>
+            <a className="text-surface-900 font-duru" href="/how">How we work</a>
+            <a className="text-surface-900 font-duru" href="/about">About</a>
+            <a className="text-surface-900 font-duru" href="/blog">Blog</a>
+            <button className="px-6 py-3 bg-gradient-to-r from-secondary-500 to-primary-500 rounded font-exo text-primary-50">
+            Reach out
+            </button>
+        </div>
+     
+      <button className=" px:3 w-6 md:hidden cursor-pointer" onClick={menu}><img src="./menu.svg"></img></button>
     </section>
   )
+
+
 }
 
 export default Navbar;
+
