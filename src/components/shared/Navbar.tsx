72660da678e27375b9c4b206c8dc53e4e116ca51
wicho1001 <wichoguerrero@yahoo.com>
2021-05-07 03:16:32 -0500

    feat: Added new navbar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,48 +1,37 @@
+import { useRouter } from 'next/router';
 import React, {useState, useEffect} from 'react';
+import Link from 'next/link'
+
 
 const Navbar = () => {
 
-  const [clickedMobile, setClickedMobile] = useState(false);
+  const router = useRouter()
 
-  useEffect(() => {
-    console.log('clickedMobile: ', clickedMobile);
-  }, [clickedMobile])
-  
+  let className = 'border-primary-500 border-b-4';
+
+
   return (
-    <section className="sticky bg-white top-0 h-20 px-6 flex justify-between items-center shadow-2xl z-2">
-      <a href="/">
-        <img src="/logo.png" alt="" className="h-15"/>
-      </a>
-   
-        <div className="xs:hidden md:flex md:items-center md:w-1/2 md:pl-15 justify-between">
-            <a className="text-surface-900 font-duru" href="/services">Services</a>
-            <a className="text-surface-900 font-duru" href="/how">How we work</a>
-            <a className="text-surface-900 font-duru" href="/about">About</a>
-            <a className="text-surface-900 font-duru" href="/blog">Blog</a>
-            <button className="px-6 py-3 bg-secondary-700 rounded font-exo text-secondary-100">
-              Reach out
-            </button>
+    <section className="flex w-full justify-between pt-8 pb-5 mb-5 px-10 sticky bg-white top-0 z-9999 shadow">
+      <Link href="/">
+        <div className="flex">
+          <img className="h-17 scale-125 transform" src="/nemiliz.png" alt="" />
+          <div className="xs:hidden md:flex flex-col ml-4 font-bold text-xl font-duru text-primary-600">
+            <p>Nemiliz</p>
+            <p>Diagnostics</p>
+          </div>
         </div>
-
-      <button className="xs:inline-block px:3 md:hidden cursor-pointer" onClick={() => setClickedMobile(!clickedMobile)}>
-        <i className="icon-menu text-4xl text-surface-600"></i>
-      </button>
-      <section className={clickedMobile ? 'xs:inline-block md:hidden absolute h-screen w-screen bg-white inset-0 flex flex-col p-6' : 'hidden'}>
-        <div className="flex w-full justify-end">
-          <button className="cursor-pointer" onClick={() => setClickedMobile(!clickedMobile)}>
-            <i className="icon-cross text-4xl text-surface-600"></i>
-          </button>
-        </div>
-        <div className="flex flex-col justify-around h-1/2 text-surface-600 font-duru text-xl font-bold">
-            <a onClick={() => setClickedMobile(!clickedMobile)} href="/services">Services</a>
-            <a onClick={() => setClickedMobile(!clickedMobile)} href="/how">How we work</a>
-            <a onClick={() => setClickedMobile(!clickedMobile)} href="/about">About</a>
-            <a onClick={() => setClickedMobile(!clickedMobile)} href="/blog">Blog</a>
-            <button className="px-6 py-3 bg-secondary-700 rounded font-exo text-secondary-100">
-              Reach out
-            </button>
-        </div>
-      </section>
+      </Link>
+      <ul className="flex">
+        <Link href="/">
+          <p className={`text-lg mx-3 ${router.pathname === '/' ? className : null}`}>Inicio</p>
+        </Link>
+        <Link href="/laboratorio">
+          <p className={`text-lg mx-3 ${router.pathname === '/laboratorio' ? className : null}`}>Laboratorio</p>
+        </Link>
+        {/* <Link href="/proyectos">
+          <p className={`text-lg mx-3 ${router.pathname === '/proyectos' ? className : null}`}>Proyectos</p>
+        </Link> */}
+      </ul>
     </section>
   )
 
