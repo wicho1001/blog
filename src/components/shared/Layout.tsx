7e850d14e3871ce3415ffd8fb9247592ddaf96e9
wicho1001 <wichoguerrero@yahoo.com>
2021-05-07 20:43:00 -0500

    feat: added header to layout component

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import Header from './Header';
 
 const Layout = (props) => {
-  
   return (
-    <section className="w-full flex flex-col">
-       <Navbar></Navbar> 
-      <div className="flex flex-col flex-grow">
-        {props.children}
-        <section className="w-full self-end">
-          <Footer></Footer>
-        </section>
-      </div>
-    </section>
+    <div>
+      <Header children={props.props}></Header>
+      <section className="w-full flex flex-col">
+        <Navbar></Navbar> 
+        <div className="flex flex-col flex-grow">
+          {props.children}
+          <section className="w-full self-end">
+            <Footer></Footer>
+          </section>
+        </div>
+      </section>
+    </div>
   )
 }
 
